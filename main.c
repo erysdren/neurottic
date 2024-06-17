f845731b449b0f1c63f9b6bfd8beb94d9e521d57
erysdren <contact@erysdren.me>
2024-06-17 17:40:48 -0500

    Initial Commit

---
+++ main.c
@@ -0,0 +1,72 @@
+
+/*
+MIT License
+
+Copyright (c) 2024 erysdren (it/she/they)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+
+#include "app.h"
+
+static int die(const char *error)
+{
+	fprintf(stderr, "%s\n", error);
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", error, NULL);
+	return -1;
+}
+
+int SDL_AppInit(void **appstate, int argc, char **argv)
+{
+	/* init sdl */
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		return die(SDL_GetError());
+
+	/* init app */
+	app_t *a = app_create("NEUROTTIC", 640, 480);
+	if (!a)
+		return die(SDL_GetError());
+
+	/* give it back to sdl */
+	*appstate = a;
+
+	return 0;
+}
+
+void SDL_AppQuit(void *appstate)
+{
+	app_t *a = (app_t *)appstate;
+	app_destroy(a);
+	SDL_Quit();
+}
+
+int SDL_AppIterate(void *appstate)
+{
+	app_t *a = (app_t *)appstate;
+	return app_iterate(a);
+}
+
+int SDL_AppEvent(void *appstate, const SDL_Event *event)
+{
+	app_t *a = (app_t *)appstate;
+	return app_event(a, event);
+}
---
+++ math.c
@@ -0,0 +1,140 @@
+
+/*
+MIT License
+
+Copyright (c) 2024 erysdren (it/she/they)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "app.h"
+
+float rad2degf(float x)
+{
+	return x * 180.0f / M_PI;
+}
+
+float deg2radf(float x)
+{
+	return x * M_PI / 180.0f;
+}
+
+float wrapf(float value, float mod)
+{
+	float cmp = (float)(value < 0);
+	return cmp * mod + SDL_fmodf(value, mod) - cmp;
+}
+
+float minf(float a, float b)
+{
+	return a < b ? a : b;
+}
+
+float maxf(float a, float b)
+{
+	return a > b ? a : b;
+}
+
+float clampf(float value, float min, float max)
+{
+	return minf(maxf(value, min), max);
+}
+
+float towardsf(float value, float change, float to)
+{
+	float diff;
+
+	if (value < to)
+		value += change;
+	else
+		value -= change;
+
+	diff = SDL_fabs(value - to);
+
+	if (diff < change)
+		value = to;
+
+	return value;
+}
+
+void vec3f_anglevectors(vec3f_t angles, vec3f_t *forward, vec3f_t *right, vec3f_t *up)
+{
+	float angle;
+	float sr, sp, sy, cr, cp, cy;
+
+	angle = deg2radf(angles.y);
+	sy = SDL_sinf(angle);
+	cy = SDL_cosf(angle);
+
+	angle = deg2radf(angles.x);
+	sp = SDL_sinf(angle);
+	cp = SDL_cosf(angle);
+
+	angle = deg2radf(angles.z);
+	sr = SDL_sinf(angle);
+	cr = SDL_cosf(angle);
+
+	if (forward)
+	{
+		forward->x = cp * cy;
+		forward->y = cp * sy;
+		forward->z = -sp;
+	}
+
+	if (right)
+	{
+		right->x = -1 * sr * sp * cy + -1 * cr * -sy;
+		right->y = -1 * sr * sp * sy + -1 * cr * cy;
+		right->z = -1 * sr * cp;
+	}
+
+	if (up)
+	{
+		up->x = cr * sp * cy + -sr * -sy;
+		up->y = cr * sp * sy + -sr * cy;
+		up->z = cr * cp;
+	}
+}
+
+vec3f_t vec3f_crossproduct(vec3f_t v1, vec3f_t v2)
+{
+	vec3f_t result;
+	result.x = v1.y * v2.z - v1.z * v2.y;
+	result.y = v1.z * v2.x - v1.x * v2.z;
+	result.z = v1.x * v2.y - v1.y * v2.x;
+	return result;
+}
+
+float vec3f_dotproduct(vec3f_t v1, vec3f_t v2)
+{
+	float result = 0.0f;
+	result += v1.x * v2.x;
+	result += v1.y * v2.y;
+	result += v1.z * v2.z;
+	return result;
+}
+
+float vec3f_normalize(vec3f_t *v)
+{
+	float w = SDL_sqrtf(v->x * v->x + v->y * v->y + v->z * v->z);
+	v->x /= w;
+	v->y /= w;
+	v->z /= w;
+	return w;
+}
